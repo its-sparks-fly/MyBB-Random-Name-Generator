530a3be53e42317ff6cba5988d2bc4cab9d3cd6e
Julia <midnightwhisper@web.de>
2019-11-28 13:38:51 +0100

    Create randomnames.lang.php

---
+++ inc/languages/deutsch_du/randomnames.lang.php
@@ -0,0 +1,13 @@
+<?php
+
+
+$l['randomnames'] = "Namenswürfel";
+$l['randomnames_gender'] = "Geschlecht";
+$l['randomnames_letter'] = "Anfangsbuchstabe";
+$l['randomnames_type'] = "Art";
+$l['randomnames_generate'] = "Namen generieren";
+$l['randomnames_pick'] = "Auswählen";
+$l['randomnames_gender_alert'] = "Nur bei Vornamen relevant.";
+$l['randomnames_prename'] = "Vorname:";
+$l['randomnames_surname'] = "Nachname:";
+?>
